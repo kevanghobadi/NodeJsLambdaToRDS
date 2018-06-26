bf9e80fbb6c640d3529424fa2e93821c57fa5be7
Kevan Ghobadi <kevang@gmail.com>
2018-06-26 14:50:38 -0500

    Adding a promise and responding correctly.

---
+++ GetUserReadings/index.js
@@ -0,0 +1,52 @@
+exports.handler = (event, context, callback) => {
+    var mysql = require('mysql');
+    var connection = mysql.createConnection({
+        host: process.env.rds_host,
+        user: process.env.rds_username,
+        password: process.env.rds_password,
+        database: process.env.db_name,
+    });
+
+
+
+    var sql = 'select * FROM customers where customers.name = (?);';
+
+    var name = event.queryStringParameters.name;
+
+    const promise = new Promise(function(resolve, reject) {
+        connection.connect(function(err) {
+            if (err) {
+                console.error('error connecting: ' + err.stack);
+                return;
+            };
+            connection.query(sql, [name], function(error, results, fields) {
+                connection.destroy();
+                resolve(results);
+                if(error){
+                    reject(error)
+                }
+            });
+        })
+    });
+    promise.then((results) => {
+        console.log(results);
+        const response = {
+            statusCode: 200,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(results)
+        };
+        callback(null, response);
+   }).catch((error) => {
+    console.log(error);
+    const response = {
+        statusCode: 500,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify("An error has occured")
+    };
+    callback(null, response);
+   });
+}
